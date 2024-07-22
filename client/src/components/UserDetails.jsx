52a9883c70dc5b207ef28300fdbdb9e9be4775d4
{iamabhi0619} <{iamabhi0619@gmail.com}>
2024-07-22 10:42:12 +0530

    Adding Responsiv

--- client/src/components/UserDetails.jsx
+++ client/src/components/UserDetails.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 function UserDetails() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState({name:"ZENVEST",userimg:"https://firebasestorage.googleapis.com/v0/b/zenvest-8f417.appspot.com/o/Designer%20(7).png?alt=media&token=1902f931-1d5f-4319-8a94-1348094d756f"});
   const [search, setSearch] = useState("");
 
   const handleSearch = async () => {
+    setData({ ...data, name: "Loading....." });
     try {
       const response = await fetch(
         `https://zenvestapi.onrender.com/newreg?id=${search}`,
@@ -44,8 +45,8 @@ function UserDetails() {
   };
 
   return (
-    <div className="justify-between bg-gradient-to-r from-cyan-500 to-blue-500 w-full min-h-screen p-10">
-      <div className=" flex bg-red-200 p-1 mb-3 border-2 border-blue-900 rounded-xl justify-between sm:justify-start">
+    <div className="justify-between bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen p-3 md:p-10">
+      <div className=" flex bg-red-200 p-1 mb-3 border-2 border-blue-900 rounded-xl justify-between sm:justify-start sm:w-fit">
         <input
           type="search"
           className="text-lg p-1 border-2 border-blue-900 rounded-lg bg-transparent mr-2"
@@ -59,18 +60,18 @@ function UserDetails() {
           Search
         </button>
       </div>
-      <div className="p-5 bg-white rounded-xl">
-        <div className="flex flex-col md:flex-row justify-start min-h-full">
-          <div className="flex flex-col justify-between md:pr-12 items-center md:items-start">
+      <div className="p-5 bg-white rounded-xl w-full sm:w-fit">
+        <div className="flex flex-col md:flex-row justify-start min-h-full sm:items-center">
+          <div className="flex flex-col justify-between md:pr-12 items-center md:items-start ">
             <div className="pb-4">
               <img
                 src={`${data.userimg}`}
-                alt="userphoto"
+                alt=""
                 className="w-32 h-32 rounded-full"
               />
             </div>
           </div>
-          <div className="flex flex-col justify-between md:pr-12 items-center md:items-start">
+          <div className="flex flex-col justify-between md:pr-12 items-center md:items-start text-center">
             <div>
               <h1 className="text-4xl text-blue-400">{data.name}</h1>
               <h3 className="text-3xl text-black-700">{data.interest}</h3>
@@ -78,10 +79,10 @@ function UserDetails() {
           </div>
         </div>
         <div>
-          <div>
-            <p className="text-2xl text-blue-800">About</p>
+          <div className="text-center  sm:text-start mb-0">
+            <p className="text-2xl text-blue-800 mb-0">About</p>
           </div>
-          <div className="flex flex-row md:p-8 border-2 border-sky-500 justify-between md:w-1/2 ">
+          <div className="flex flex-row p-1 md:p-8 border-2 border-sky-500 justify-between md:w-full mt-0">
             <div className="text-lg truncate mr-2">
               <p>Registration ID:</p>
               <p>Date-of-Birth:</p>
@@ -92,11 +93,11 @@ function UserDetails() {
             </div>
             <div className="text-lg truncate">
               <p>{data.id}</p>
-              <p>{new Date(data.dateofbirth).toDateString("en-IN")}</p>
+              <p>{data.dateofbirth? `${new Date(data.dateofbirth).toDateString("en-IN")}`: ""}</p>
               <p>{data.gender}</p>
               <p>{data.number}</p>
               <p>{data.course}</p>
-              <p>{new Date(data.date).toDateString("en-IN")}</p>
+              <p>{data.date? `${new Date(data.date).toDateString("en-IN")}` : ""}</p>
             </div>
           </div>
         </div>
