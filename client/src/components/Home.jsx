1225b79882d16dc92a7c84bb18504fc611a131e0
{iamabhi0619} <{iamabhi0619@gmail.com}>
2024-07-24 10:46:19 +0530

    home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='bg-gradient-to-r from-sky-500 to-indigo-500 flex justify-center items-center min-h-screen'>
+      <div className='flex flex-col items-center'>
+        <a href="/"><img src="https://firebasestorage.googleapis.com/v0/b/zenvest-8f417.appspot.com/o/Designer__7_-removebg-preview.png?alt=media&token=48021a86-857a-46a4-be89-0d130ce713c7" alt="" /></a>
+      </div>
+    </div>
   )
 }
 
