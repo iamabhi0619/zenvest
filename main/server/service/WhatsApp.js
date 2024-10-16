b20feb39989b9d6fbcee6fb19f467f4299210f15
Abhishek Kumar <131546128+iamabhi0619@users.noreply.github.com>
2024-10-16 10:31:43 +0530

    WhatsApp Message update

--- main/server/service/WhatsApp.js
+++ main/server/service/WhatsApp.js
@@ -17,8 +17,7 @@ const sendRegMessage = async (data) => {
     let { number } = data;
     number = number.toString();
     number = formatPhoneNumber(number);
-    const message = `*Hey ${data.name}*\nThank you for registering as a member of Zenvest LPU!\nWe are thrilled to have you, our vibrant community dedicated to technology, finance, entrepreneurship, and building a secure future.
-We are excited to inform you that we will soon be scheduling an interview as part of our onboarding process. We will provide all the details shortly.\nThank you for your support and active participation.`;
+    const message = `Dear ${data.name},\n\nThank you for registering as a member of Zenvest LPU!\n\nWe are delighted to welcome you to our vibrant community dedicated to technology, finance, entrepreneurship, and building a secure future. Your enthusiasm and support are valuable to us, and we look forward to seeing the positive impact you will bring.\n\nAs part of our onboarding process, we will be scheduling an interview with you shortly. Further details will be shared soon, so please stay tuned!\n\nIn the meantime, feel free to connect with us on our social media platforms to stay updated and engage with our community:\nInstagram: https://www.instagram.com/zenvest_lpu?igsh=NjlpZzB5eG1mOTdy\nLinkedIn: https://www.linkedin.com/company/zenvest-lpu/\nThank you once again for your active participation. We are excited to grow and achieve great things together!\n\nBest regards,\nZenvest'LPU`;
     const body = {
       token: apiKey,
       to: `${number}`,
