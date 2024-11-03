e6426e4ecb0ff3b51e2d341b13b8a23f9462ed43
Abhishek Kumar <131546128+iamabhi0619@users.noreply.github.com>
2024-11-03 18:37:48 +0530

    Responsive designe

--- finathone/client/src/App.js
+++ finathone/client/src/App.js
@@ -1,44 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { CheckOut } from "./components/CheckOut";
 import EventRegistrationForm from "./components/EventRegistrationForm";
 
 function App() {
   const [data, setData] = useState({});
-
   const setdata = (e) => {
     setData(e);
   };
 
-  // Set the --vh property for mobile browsers
-  const setVh = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
-
-  useEffect(() => {
-    setVh();
-    window.addEventListener('resize', setVh);
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
-
   return (
-    <div className="flex flex-col max-h-screen items-center bg-[#3e688c] p-3 justify-between min-h-screen app">
-      <header className="w-full text-white text-center font-semibold text-xl flex flex-col items-center justify-center">
+    <>
+      <header className="w-full text-white text-center font-semibold text-xl flex flex-col items-center justify-center h-[10vh]">
         <p className="text-5xl font-outline font-bold md:tracking-widest outline-1 outline-white">
           FIN-A-THON
         </p>
       </header>
-      <main className="flex-grow w-full flex items-center justify-center">
+      <main className="w-full flex items-center justify-center h-[80vh] overflow-auto">
         {Object.keys(data).length === 0 ? (
           <EventRegistrationForm submit={setdata} />
         ) : (
           <CheckOut data={data} />
         )}
       </main>
-      <footer className="w-full p-4 text-white text-center text-sm">
+      <footer className="w-full p-4 text-white text-center text-sm h-[10vh]">
         © 2024 ZENVEST. All rights reserved.
       </footer>
-    </div>
+    </>
   );
 }
 
