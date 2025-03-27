ada1d67deae40057296f473d50fb18cd0958a7d4
Abhishek Kumar <131546128+iamabhi0619@users.noreply.github.com>
2025-03-27 11:13:01 +0530

    Remove the node

--- serverold/routes/finathone.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const {getUserData, certificate} = require("../controller/finathone")
-
-const router = express.Router();
-
-router.get("/:id",getUserData);
-router.get('/ap/certificate', certificate)
-
-module.exports = router;
